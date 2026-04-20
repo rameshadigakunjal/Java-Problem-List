4a03d2d7ac2bb7db0fd7d8531ea88cc7bc460f85
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-04-20 14:16:20 +0530

    Day-22

---
+++ src/GenerateParentheses.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
+
+    private void backtrack(List<String> result, String current, int open, int close, int n) {
+        // Base case
+        if (current.length() == 2 * n) {
+            result.add(current);
+            return;
+        }
+
+        // Add '('
+        if (open < n) {
+            backtrack(result, current + "(", open + 1, close, n);
+        }
+
+        // Add ')'
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, n);
+        }
+    }
+    public static void main(String[] args) {
+        GenerateParentheses solution = new GenerateParentheses();
+        System.out.println(solution.generateParenthesis(3));    
+        
+    }
+}
