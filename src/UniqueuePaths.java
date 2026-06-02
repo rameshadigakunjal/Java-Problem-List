2525759674d22f2a2e6a10e0039353d1d432f201
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2026-06-02 15:51:21 +0530

    Day-62

---
+++ src/UniqueuePaths.java
@@ -0,0 +1,26 @@
+class UniqueuePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+
+        // First row = 1
+        for (int i = 0; i < m; i++) dp[i][0] = 1;
+
+        // First column = 1
+        for (int j = 0; j < n; j++) dp[0][j] = 1;
+
+        // Fill rest
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+    public static void main(String[] args) {
+        UniqueuePaths solution = new UniqueuePaths();
+        int m = 3, n = 7; // Example input
+        int result = solution.uniquePaths(m, n);
+        System.out.println(result); // Output: 28   
+    }
+}
